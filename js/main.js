9d67790d0d688aa1b96fae90e0a9bf6ddfaa9e58
Oliver0731 <o.townell@share.epsb.ca>
2024-04-12 09:45:13 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-let verbs = loadTextFile("wordData/Verbs.txt");
-function list(array) {
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-  }
+let outputEl = document.getElementById("output");
+
+let nouns = await loadTextFile("wordData/nouns.txt");
+let verbs = await loadTextFile("worddata/verbs.txt");
+let adjectives = await loadTextFile("wordData/adjective.txt");
+
+function generate() {
+  let noun = nouns[randomInt(0, 1001)];
+  let verb = verbs[randomInt(0, 1001)];
+  let adjective = adjectives[randomInt(0, adjectives.length)];
+  outputEl.innerHTML = `dear ${
+    nouns[randomInt(0, 1001)]
+  }, i would very much like to ${verb} you for reaserch. I will ${verb} your ${adjective} ${noun} but only in the case that your ${adjective} ${noun} will ${verb} yourself.`;
 }
-list(verbs);
-let test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+generate();
