3a9866de90f0c6c9ddc883c60a19638ccde141f5
Oliver0731 <o.townell@share.epsb.ca>
2024-04-12 08:48:30 -0600

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let verbs = loadTextFile("wordData/Verbs.txt");
+function list(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+list(verbs);
+let test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
